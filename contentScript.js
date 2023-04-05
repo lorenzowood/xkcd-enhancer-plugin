350ac7f0e93494d22804a0ba5017f0f977fd09e1
lorenzowood <lorenzo@lorenzowood.com>
2023-04-05 23:52:05 +0200

    Initial commit

---
+++ contentScript.js
@@ -0,0 +1,50 @@
+(function () {
+    if (window.location.hostname !== "xkcd.com") return;
+  
+    const floatingDiv = document.createElement("div");
+    floatingDiv.id = "xkcd-navigator";
+  
+    const prevButton = document.createElement("button");
+    prevButton.id = "prevButton";
+    prevButton.textContent = "Previous";
+  
+    const nextButton = document.createElement("button");
+    nextButton.id = "nextButton";
+    nextButton.textContent = "Next";
+  
+    floatingDiv.appendChild(prevButton);
+    floatingDiv.appendChild(nextButton);
+  
+    document.body.appendChild(floatingDiv);
+  
+    const prevLink = document.querySelector('a[rel="prev"]');
+    const nextLink = document.querySelector('a[rel="next"]');
+  
+    if (prevLink) {
+      prevButton.addEventListener("click", () => {
+        window.location.href = prevLink.href;
+      });
+    } else {
+      prevButton.disabled = true;
+    }
+  
+    if (nextLink) {
+      nextButton.addEventListener("click", () => {
+        window.location.href = nextLink.href;
+      });
+    } else {
+      nextButton.disabled = true;
+    }
+  
+    const comicDiv = document.querySelector("#comic");
+    const comicImg = comicDiv && comicDiv.querySelector("img");
+  
+    if (comicImg && comicImg.title) {
+      const titleDiv = document.createElement("div");
+      titleDiv.id = "xkcd-title";
+      titleDiv.textContent = comicImg.title;
+  
+      comicImg.insertAdjacentElement("afterend", titleDiv);
+    }
+  })();
+  
